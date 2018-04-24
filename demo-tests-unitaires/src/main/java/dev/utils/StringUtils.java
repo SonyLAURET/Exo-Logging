711aba54018c65a08b2d4a18f2915b841c4d4079
Sony LAURET <sony.lauret@gmail.com>
2018-04-24 14:38:59 +0200

    create test

--- demo-tests-unitaires/src/main/java/dev/utils/StringUtils.java
+++ demo-tests-unitaires/src/main/java/dev/utils/StringUtils.java
@@ -1,12 +1,18 @@
 package dev.utils;
 
-/** Classe qui fournit des services de traitements de chaines de caractères
+/**
+ * Classe qui fournit des services de traitements de chaines de caractères
+ * 
  * @author DIGINAMIC
  */
 public final class StringUtils {
-	/** Retourne la distance de Levenshtein entre 2 chaines de caractères* 
-	 * @param lhs chaine 1
-	 * @param rhs chaine 2
+	/**
+	 * Retourne la distance de Levenshtein entre 2 chaines de caractères*
+	 * 
+	 * @param lhs
+	 *            chaine 1
+	 * @param rhs
+	 *            chaine 2
 	 * @return distance
 	 */
 	public static int levenshteinDistance(CharSequence lhs, CharSequence rhs) {
@@ -15,28 +21,32 @@ public final class StringUtils {
 
 		int[] cost = new int[len0];
 		int[] newcost = new int[len0];
+		if (!(lhs.equals(null))) {
 
-		for (int i = 0; i < len0; i++) {
-			cost[i] = i;
-		}
-
-		for (int j = 1; j < len1; j++) {
-			newcost[0] = j;
-
-			for (int i = 1; i < len0; i++) {
-				int match = (lhs.charAt(i - 1) == rhs.charAt(j - 1)) ? 0 : 1;
-
-				int costReplace = cost[i - 1] + match;
-				int costInsert = cost[i] + 1;
-				int costDelete = newcost[i - 1] + 1;
-
-				newcost[i] = Math.min(Math.min(costInsert, costDelete), costReplace);
+			for (int i = 0; i < len0; i++) {
+				cost[i] = i;
 			}
 
-			int[] swap = cost;
-			cost = newcost;
-			newcost = swap;
+			for (int j = 1; j < len1; j++) {
+				newcost[0] = j;
+
+				for (int i = 1; i < len0; i++) {
+					int match = (lhs.charAt(i - 1) == rhs.charAt(j - 1)) ? 0 : 1;
+
+					int costReplace = cost[i - 1] + match;
+					int costInsert = cost[i] + 1;
+					int costDelete = newcost[i - 1] + 1;
+
+					newcost[i] = Math.min(Math.min(costInsert, costDelete), costReplace);
+				}
+
+				int[] swap = cost;
+				cost = newcost;
+				newcost = swap;
+			}
 		}
+
 		return cost[len0 - 1];
+
 	}
 }
---
+++ demo-tests-unitaires/src/test/java/testDemoTestUnitaire.java
@@ -0,0 +1,47 @@
+import static org.junit.Assert.assertThat;
+
+import dev.utils.StringUtils;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/** Unit test for simple App. */
+public class testDemoTestUnitaire extends TestCase {
+
+	/**
+	 * Create the test case
+	 * 
+	 * @param testName
+	 *            name of the test case
+	 */
+	public testDemoTestUnitaire() {
+		super();
+	}
+
+	/** @return the suite of tests being tested */
+	public static Test suite() {
+		return new TestSuite(testDemoTestUnitaire.class);
+	}
+
+	/** Rigourous Test */
+	@org.junit.Test
+	public void testAppEquals1() {
+		assertEquals(StringUtils.levenshteinDistance("chat", "chats"), 1);
+		assertEquals(StringUtils.levenshteinDistance("machins", "machine"), 1);
+		assertEquals(StringUtils.levenshteinDistance("aviron", "avion"), 1);
+	}
+
+	public void testAppEquals2() {
+		assertEquals(StringUtils.levenshteinDistance("distance", "instance"), 2);
+		assertEquals(StringUtils.levenshteinDistance("Chien", "Chine"), 2);
+	}
+
+	public void testAppDifferent() {
+		assertEquals(StringUtils.levenshteinDistance("distance", "e"), 7);
+
+	}
+	public void testAppNull() {
+		assertEquals(StringUtils.levenshteinDistance("distance", null), 7);
+
+	}
+}
